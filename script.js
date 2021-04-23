126270bb03d5312fa78c2cff1e72a51ec049cec1
veerpravath <veerpravath@gmail.com>
2021-04-23 11:12:23 +0530

    sidenav

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const showBtn
+const showBtn=document.querySelector('.navBtn');
+const topNav=document.querySelector('.top-nav');
+
+showBtn.addEventListener('click',function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML='<i class="fas fa-bars"></li>';
+    }else{
+        topNav.classList.add('showNav');
+        showBtn.innerHTML='<i class="fas fa-times"></li>';
+    }
+});
